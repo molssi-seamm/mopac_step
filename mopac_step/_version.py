9e15ee09213041fc34ad51f64bd787b37f817a91
Paul Saxe <paul@thesaxes.net>
2019-08-13 15:41:39 -0400

    Fixing some lingering 'forcefield' words left from copying

--- mopac_step/_version.py
+++ mopac_step/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "forcefield_step/_version.py"
+    cfg.versionfile_source = "mopac_step/_version.py"
     cfg.verbose = False
     return cfg
 
