3ccecd281c0ca8c146a9e1a63679b60b7e104793
Paul Saxe <paul@thesaxes.net>
2021-09-16 06:18:46 -0600

    Added GUI for structure handling.

--- mopac_step/tk_optimization.py
+++ mopac_step/tk_optimization.py
@@ -5,33 +5,22 @@
 import mopac_step
 import seamm_widgets as sw
 import tkinter as tk
+import tkinter.ttk as ttk
 
 
 class TkOptimization(mopac_step.TkEnergy):
 
     def __init__(
-        self,
-        tk_flowchart=None,
-        node=None,
-        canvas=None,
-        x=120,
-        y=20,
-        w=200,
-        h=50
+        self, tk_flowchart=None, node=None, canvas=None, x=120, y=20, w=200, h=50
     ):
         '''Initialize a node
 
         Keyword arguments:
         '''
         super().__init__(
-            tk_flowchart=tk_flowchart,
-            node=node,
-            canvas=canvas,
-            x=x,
-            y=y,
-            w=w,
-            h=h
+            tk_flowchart=tk_flowchart, node=node, canvas=canvas, x=x, y=y, w=w, h=h
         )
+        self.mopac_parameters = self.node.parent.parameters
 
     def right_click(self, event):
         """Probably need to add our dialog...
@@ -42,13 +31,31 @@ class TkOptimization(mopac_step.TkEnergy):
 
         self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
 
-    def create_dialog(
-        self, title='MOPAC Optimization', calculation='optimization'
-    ):
+    def create_dialog(self, title='MOPAC Optimization', calculation='optimization'):
         """Create the dialog!"""
         self.logger.debug('Creating the dialog')
-        super().create_dialog(title=title, calculation='optimization')
+
+        frame = super().create_dialog(title=title, calculation='optimization')
+
+        P = self.node.parameters
+
+        # Create the structure-handling widgets
+        sframe = self["structure frame"] = ttk.LabelFrame(
+            frame, text="Configuration Handling", labelanchor=tk.N
+        )
+        row = 0
+        widgets = []
+        for key in ("structure handling", "configuration name"):
+            self[key].destroy()
+            self[key] = P[key].widget(sframe)
+            self[key].grid(row=row, column=0, sticky=tk.EW)
+            widgets.append(self[key])
+            row += 1
+        sw.align_labels(widgets)
+
         self.logger.debug('Finished creating the dialog')
+
+        return frame
 
     def reset_dialog(self, widget=None):
         """Layout the widgets in the main frame
@@ -83,5 +90,8 @@ class TkOptimization(mopac_step.TkEnergy):
 
         sw.align_labels(widgets)
         sw.align_labels(widgets_2)
+
+        self["structure frame"].grid(row=row, column=0, columnspan=2, sticky=tk.EW)
+        row += 1
 
         return row
